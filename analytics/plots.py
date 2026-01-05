c7f3233fb70fdbe64c2e5472690eb54ec21a0e44
rushikeshbathe096 <rushikeshbathe096@gmail.com>
2026-01-05 05:33:48 +0530

    Basic visualisation added using matplotlib

--- analytics/plots.py
+++ analytics/plots.py
@@ -1,6 +1,14 @@
 import matplotlib.pyplot as plt
+import os
+
+OUTPUT_DIR = "plots"
+
+def ensure_dir():
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
 
 def plot_moves(move_stats):
+    ensure_dir()
     moves = list(move_stats.keys())
     counts = list(move_stats.values())
 
@@ -9,9 +17,11 @@ def plot_moves(move_stats):
     plt.title("Player Move Frequency")
     plt.xlabel("Move")
     plt.ylabel("Count")
-    plt.show()
+    plt.savefig(f"{OUTPUT_DIR}/move_frequency.png")
+    plt.close()
 
 def plot_results(result_stats):
+    ensure_dir()
     results = list(result_stats.keys())
     counts = list(result_stats.values())
 
@@ -20,4 +30,5 @@ def plot_results(result_stats):
     plt.title("Game Results")
     plt.xlabel("Result")
     plt.ylabel("Count")
-    plt.show()
+    plt.savefig(f"{OUTPUT_DIR}/game_results.png")
+    plt.close()
